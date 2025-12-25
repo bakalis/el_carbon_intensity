dd14d641acd19f6c4757c35a94650ae9383bef77
bakalis <bakalis@kth.se>
2025-12-25 03:07:08 +0100

    Update map to change between the whole sweden or the regions

--- view/src/test_app/main.cljs
+++ view/src/test_app/main.cljs
@@ -1,27 +1,28 @@
 (ns test-app.main
   (:require [reagent.core :as r]
             [reagent.dom.client :as rdomc]
-            ["react" :as react]
             ["react-leaflet" :refer [MapContainer TileLayer Marker Popup GeoJSON]]
-            ["leaflet" :as L]
             [data.SE-1 :as SE-1]
             [data.SE-2 :as SE-2]
             [data.SE-3 :as SE-3]
-            [data.SE-4 :as SE-4]))
+            [data.SE-4 :as SE-4]
+            [data.SE :as SE]))
 
 ;; App state
-(defonce app-state (r/atom {:center [59.3293 18.0686]
+(defonce app-state (r/atom {:center [62.3293 18.0686]
                             :zoom 5
-                            :geojson-layers [SE-1/data
-                                             SE-2/data
-                                             SE-3/data
-                                             SE-4/data]}))
+                            :region-mode :zones
+                            :geojson [SE-1/data 
+                                      SE-2/data 
+                                      SE-3/data 
+                                      SE-4/data]}))
 
 ;; Root element for React 19
 (defonce root (atom nil))
 
-(defn geojson-style [layer-index]
-  (fn [feature]
+(defn geojson-style 
+  [layer-index]
+  (fn [_]
     (let [colors ["#3388ff" "#ff6b6b" "#4ecdc4" "#ffe66d"]]
       (clj->js {:fillColor (get colors layer-index "#3388ff")
                 :weight 2
@@ -53,6 +54,51 @@
    [:h3 "Error Loading Data"]
    [:p error]])
 
+(defn set-region-mode! [mode]
+  (swap! app-state
+         (fn [state]
+           (case mode
+             :full
+             (-> state
+                 (assoc :region-mode :full)
+                 (assoc :geojson [SE/data]))
+
+             :zones
+             (-> state
+                 (assoc :region-mode :zones)
+                 (assoc :geojson [SE-1/data 
+                                  SE-2/data 
+                                  SE-3/data 
+                                  SE-4/data]))))))
+
+(defn regions-selector []
+  (let [mode (:region-mode @app-state)]
+    [:div.regions-selector
+     {:style {:margin "1rem 0"
+              :display "flex"
+              :align-items "center"
+              :justify-content "center"}}
+
+     [:h3 "Regions"]
+
+     [:label
+      {:style {:display "block" :cursor "pointer"}}
+      [:input
+       {:type "radio"
+        :name "regions"
+        :checked (= mode :full)
+        :on-change #(set-region-mode! :full)}]
+      " Full Sweden"]
+
+     [:label
+      {:style {:display "block" :cursor "pointer"}}
+      [:input
+       {:type "radio"
+        :name "regions"
+        :checked (= mode :zones)
+        :on-change #(set-region-mode! :zones)}]
+      " Zones"]]))
+
 ;; Map component
 (defn map-component []
   [:div.map-container
@@ -70,23 +116,18 @@
 
      ;; Optional: hide zoom controls
      :zoomControl false
-     :style {:height "80vh" :width "60vw"}}
+     :style {:height "90vh" :width "80vw"}}
     [:> TileLayer
      {:attribution "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
       :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]
 
     ;; Render each GeoJSON layer
-    (for [[idx geojson] (map-indexed vector (:geojson-layers @app-state))]
-      ^{:key idx}
+    (for [[idx geojson] (map-indexed vector (:geojson @app-state))]
+      ^{:key (str idx "-" (hash geojson))}  ;; 🔑 force remount
       [:> GeoJSON
        {:data (clj->js geojson)
         :style (geojson-style idx)
-        :onEachFeature on-each-feature}])
-
-    [:> Marker
-     {:position (clj->js (:center @app-state))}
-     [:> Popup
-      "Center marker!"]]]])
+        :onEachFeature on-each-feature}])]])
 
 ;; Main app
 (defn app []
@@ -94,6 +135,7 @@
    [:header
     [:h1 "React-Leaflet ClojureScript App"]
     [:p "Map with multiple GeoJSON layers"]]
+   [regions-selector]
    (cond
      (:loading @app-state) [loading-spinner]
      (:error @app-state) [error-message (:error @app-state)]
