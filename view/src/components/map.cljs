420a66f379346e4044249eeee033118027f6fa24
bakalis <bakalis@kth.se>
2026-01-10 16:30:25 +0100

    Tooltip showing numbers on hover

--- view/src/components/map.cljs
+++ view/src/components/map.cljs
@@ -2,7 +2,8 @@
   (:require ["react-leaflet" :refer [MapContainer TileLayer GeoJSON]]
             [utils.common :refer [intensity->color]]
             [utils.datetime :refer [index-intensities-by-hour]]
-            [state.state :refer [app-state open-modal!]]))
+            [state.state :refer [app-state open-modal!]]
+            [clojure.string :as str]))
 
 (defn geojson-style
   [intensities]
@@ -24,12 +25,36 @@
          :dashArray   "3"
          :fillOpacity 0.7}))))
 
-;; Function called when a feature is clicked
-(defn on-each-feature [feature layer]
-  (.on layer "click" 
-       (fn [e]
-         (.stopPropagation (.-originalEvent e))
-         (open-modal! (js->clj feature :keywordize-keys true)))))
+;; Function called when a feature is added
+(defn on-each-feature [intensities]
+  (fn [feature layer]
+    (let [props (js->clj (.-properties feature) :keywordize-keys true)
+          region-id (:zoneName props)]
+      
+      (.on layer "click" 
+           (fn [e]
+             (.stopPropagation (.-originalEvent e))
+             (open-modal! (js->clj feature :keywordize-keys true))))
+      
+      (.bindTooltip layer
+                    (fn []
+                      (let [selected-hour (:selected-hour @app-state)
+                            intensity-type (name (:intensity-type @app-state))
+                            intensities-by-datetime (index-intensities-by-hour intensities)
+                            actual (get-in intensities-by-datetime [region-id intensity-type selected-hour "actual_intensity"]) 
+                            predicted (get-in intensities-by-datetime [region-id intensity-type selected-hour "predicted_intensity"])
+                            ci (or actual predicted)
+                            data-type (if actual "Actual" "Predicted")]
+                        (str "<div style='padding: 8px;'>"
+                             "<strong style='font-size: 14px;'>" region-id "</strong><br/>"
+                             "<span style='font-size: 12px; color: #666;'>" data-type " " (str/capitalize intensity-type) " Intensity</span><br/>"
+                             "<strong style='font-size: 16px; color: " (intensity->color ci) ";'>"
+                             (if ci (.toFixed ci 2) "N/A") " gCO₂/kWh"
+                             "</strong>"
+                             "</div>")))
+                    #js {:sticky true
+                         :direction "top"
+                         :className "custom-tooltip"}))))
 
 (defn map-component []
   (let [{:keys [center carbon-intensities zoom geojson]} @app-state]
@@ -45,18 +70,16 @@
        :touchZoom false
        :boxZoom false
        :keyboard false
-
        :zoomControl false
        :style {:height "90vh" :width "80vw"}
        :key (str "map-" (hash carbon-intensities))}
       [:> TileLayer
        {:attribution "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
         :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]
-
       ;; Render each GeoJSON layer
       (for [[idx geojson] (map-indexed vector geojson)]
-        ^{:key (str idx "-" (hash geojson))}  ;; 🔑 force remount
+        ^{:key (str idx "-" (hash geojson))}
         [:> GeoJSON
          {:data (clj->js geojson)
           :style (geojson-style carbon-intensities)
-          :onEachFeature on-each-feature}])]]))
+          :onEachFeature (on-each-feature carbon-intensities)}])]]))
